c8ba176ff823921fa322ad3998e6500dde006d9f
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-08-15 15:23:42 +0100

    Add form markup to the Pizza Razor page

--- Pages/Pizza.cshtml.cs
+++ Pages/Pizza.cshtml.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPagesPizza.Pages
 {
-    public class PizzaModel : PageModel
+  public class PizzaModel : PageModel
+  {
+    [BindProperty]
+    public Pizza NewPizza { get; set; }
+    
+    public void OnGet()
     {
-        public void OnGet()
-        {
-        }
     }
+  }
 }
